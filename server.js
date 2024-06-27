6d98c3703d9bee3df9c4d57774fdf74632ddcf6f
BookyDis <wnaphon@gmail.com>
2024-06-27 21:33:00 +0700

    basic layout pug and installed daisyui

--- server.js
+++ server.js
@@ -1,4 +1,17 @@
 const express = require('express')
+
+
+var indexRouter = require('./routes/index');
+
 const app = express()
+// view engine setup
+app.set('view engine', 'pug');
+
+app.get("/", (req, res) => {
+    res.render('index')
+})
+app.use('/index', indexRouter);
 
 app.listen(3000)
+
+module.exports = app;
